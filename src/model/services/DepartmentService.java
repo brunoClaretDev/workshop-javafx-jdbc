db7ec970f8a6d31d1d9a4c2c30df99ea5ae66e5b
brunoClaretDev <brunoclaret.dev@gmail.com>
2026-08-03 23:01:24 -0300

    Remove Department

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -14,14 +14,17 @@ public class DepartmentService {
 		return dao.findAll();
 
 	}
-	
+
 	public void saveOrUpdate(Department obj) {
 		if (obj.getId() == null) {
 			dao.insert(obj);
-		}
-		else {
+		} else {
 			dao.update(obj);
 		}
 	}
-	
+
+	public void remove(Department obj) {
+		dao.deleteById(obj.getId());
+	}
+
 }
